407e020e8bb6f098702daf577c1ae4724d42b80e
MostofaAsik <mmostofaasik@gmail.com>
2023-05-19 15:18:19 +0600

    singleData

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -39,9 +39,18 @@ async function run() {
         // get >> all
         app.get('/allToys', async (req, res) => {
             const cursor = toysCollection.find()
-            const result = await cursor.toArray()
+            const result = await cursor.limit(20).toArray()
             res.send(result)
         })
+
+        // get:signleidData  
+        app.get('/allToys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await toysCollection.findOne(query)
+            res.send(result)
+        })
+
 
 
         // Send a ping to confirm a successful connection
