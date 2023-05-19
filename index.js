0a0607fead3633f70f5a8bc90b46830b8a93297f
MostofaAsik <mmostofaasik@gmail.com>
2023-05-19 09:21:05 +0600

    post method

--- index.js
+++ index.js
@@ -25,6 +25,25 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const toysCollection = client.db("toysDb").collection("toys")
+
+
+        //post
+        app.post('/allToys', async (req, res) => {
+            const toy = req.body;
+            const result = await toysCollection.insertOne(toy)
+            res.send(result)
+        })
+
+        //get>>all
+        app.get('/allToys', async (req, res) => {
+            const cursor = toysCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
