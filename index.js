810972cc111cb25ce1e30f60549af92e1f558f55
MostofaAsik <mmostofaasik@gmail.com>
2023-05-20 07:06:10 +0600

    vercel json

--- index.js
+++ index.js
@@ -18,13 +18,16 @@ const client = new MongoClient(uri, {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
+    },
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // maxPoolSize: 10,
 });
 
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect()
 
         const toysCollection = client.db("toysDb").collection("toys")
 
